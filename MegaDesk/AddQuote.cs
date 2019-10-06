7b183fd3b4c2c88097cd79fefcd2358e0695c689
Marvin Enamorado <soycatracho@hotmail.com>
2019-10-05 21:24:11 -0600

    cchanges made

--- MegaDesk/AddQuote.cs
+++ MegaDesk/AddQuote.cs
@@ -23,5 +23,55 @@ namespace MegaDesk
             viewMainMenu.Show();
             Close();
         }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeskWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void DeskWidth_Validating(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void AddQuote_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeskWidth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+        }
     }
 }
